7480e6edf3cb8effc9eb5c49300362ad1f882075
Gtio <gtoxlili@outlook.com>
2025-05-15 12:01:07 +0800

    refactor SQL constants and enhance table management methods in store

--- constants.go
+++ constants.go
@@ -1,7 +1,7 @@
 package pgxadapter
 
 const (
-	createTableSQL = `
+	createTable = `
 		create table if not exists %[1]s (
 			id int generated always as identity primary key,
 			ptype text not null,
@@ -10,17 +10,15 @@ const (
 		create unique index if not exists uk_%[1]s on %[1]s (ptype, %[3]s)
 	`
 
-	insertRowSQL = "insert into %[1]s (ptype, %[2]s) values (%[3]s) on conflict (ptype, %[2]s) do nothing"
+	insertRow = "insert into %[1]s (ptype, %[2]s) values (%[3]s) on conflict (ptype, %[2]s) do nothing"
 
-	updateRowSQL = "update %[1]s set ptype = $1, %[2]s where ptype = $3 and %[3]s"
+	updateRow = "update %[1]s set ptype = $1, %[2]s where ptype = $3 and %[3]s"
 
 	deleteAll = "truncate table %[1]s restart identity"
 
 	deleteRow = "delete from %[1]s where ptype = $1 and %[2]s"
 
-	deleteByArgs = "delete from %[1]s where ptype = $1"
+	deleteByPType = "delete from %[1]s where ptype = $1"
 
-	selectAll = "select ptype, %[2]s from %[1]s"
-
-	selectWhere = "select ptype, %[2]s from %[1]s where %[3]s"
+	selectSQL = "select ptype, %[2]s from %[1]s"
 )
--- store.go
+++ store.go
@@ -36,14 +36,18 @@ func (s *store) setFieldCount(fieldCount int) {
 }
 
 func (s *store) setTableName(tableName string) {
-	s.tableName = tableName
+	s.tableName = lo.SnakeCase(tableName)
 }
 
 func (s *store) initTable(ctx context.Context) error {
+	return s.createTable(ctx)
+}
+
+func (s *store) createTable(ctx context.Context) error {
 	vColumns := lo.Times(s.fieldCount, func(i int) string {
 		return "v" + strconv.Itoa(i)
 	})
-	sqlSeq := strings.SplitSeq(fmt.Sprintf(createTableSQL, lo.SnakeCase(s.tableName), strings.Join(lo.Map(vColumns, func(v string, _ int) string {
+	sqlSeq := strings.SplitSeq(fmt.Sprintf(createTable, s.tableName, strings.Join(lo.Map(vColumns, func(v string, _ int) string {
 		return v + " text not null"
 	}), ","), strings.Join(vColumns, ",")), ";")
 
@@ -63,4 +67,71 @@ func (s *store) initTable(ctx context.Context) error {
 		}
 	}
 	return nil
+}
+
+func (s *store) dropTable(ctx context.Context) error {
+	sql := fmt.Sprintf(deleteAll, s.tableName)
+	_, err := s.db.Exec(ctx, sql)
+	if err != nil {
+		return fmt.Errorf("failed to drop table: %v", err)
+	}
+	return nil
+}
+
+func (s *store) insertRow(ctx context.Context, args ...string) error {
+	if len(args) != s.fieldCount+1 {
+		return fmt.Errorf("args length %d is not equal to field count %d", len(args)-1, s.fieldCount)
+	}
+
+	sql := fmt.Sprintf(insertRow, s.tableName, strings.Join(lo.Times(s.fieldCount, func(i int) string {
+		return "v" + strconv.Itoa(i)
+	}), ","), strings.Join(lo.Times(s.fieldCount, func(i int) string {
+		return "$" + strconv.Itoa(i+1)
+	}), ","))
+
+	_, err := s.db.Exec(ctx, sql, lo.ToAnySlice(args)...)
+	if err != nil {
+		return fmt.Errorf("failed to insert row: %v", err)
+	}
+	return nil
+}
+
+func (s *store) selectAll(ctx context.Context) ([][]string, error) {
+	return s.selectWhere(ctx, "", 0)
+}
+
+func (s *store) selectWhere(ctx context.Context, ptype string, startIdx int, args ...string) ([][]string, error) {
+	sql := fmt.Sprintf(selectSQL, s.tableName, strings.Join(lo.Times(s.fieldCount, func(i int) string {
+		return "v" + strconv.Itoa(i)
+	}), ","), ptype)
+
+	var conditions []string
+	if lo.IsNotEmpty(ptype) {
+		conditions = append(conditions, "ptype = $1")
+		args = append([]string{ptype}, args...)
+	}
+	conditions = append(conditions, lo.Map(lo.Filter(lo.Map(args, func(arg string, i int) string {
+		return "v" + strconv.Itoa(i+startIdx)
+	}), func(_ string, i int) bool {
+		return lo.IsEmpty(args[i])
+	}), func(arg string, i int) string {
+		return arg + " = $" + strconv.Itoa(i+1+len(conditions))
+	})...)
+
+	rows, err := s.db.Query(ctx, sql, lo.ToAnySlice(lo.Compact(args))...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to select where: %v", err)
+	}
+	defer rows.Close()
+
+	var result [][]string
+	for rows.Next() {
+		row := lo.ToSlicePtr(make([]string, s.fieldCount+1))
+		err := rows.Scan(lo.ToAnySlice(row)...)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan row: %v", err)
+		}
+		result = append(result, lo.FromSlicePtr(row))
+	}
+	return result, nil
 }
