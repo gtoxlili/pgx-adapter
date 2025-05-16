7e6b53bdf9c7fa3a0de66ea4ce74f8ea59a654aa
Gtio <gtoxlili@outlook.com>
2025-05-16 11:23:06 +0800

    fix: update filter type in LoadFilteredPolicyCtx and enhance error message for clarity

--- impl_filtered.go
+++ impl_filtered.go
@@ -6,6 +6,7 @@ import (
 	"github.com/casbin/casbin/v2/model"
 	"github.com/casbin/casbin/v2/persist"
 	"github.com/samber/lo"
+	"strings"
 )
 
 func (a *Adapter) LoadFilteredPolicyCtx(ctx context.Context, model model.Model, filter interface{}) error {
@@ -13,18 +14,26 @@ func (a *Adapter) LoadFilteredPolicyCtx(ctx context.Context, model model.Model, 
 		return a.LoadPolicyCtx(ctx, model)
 	}
 	a.filter.Store(true)
-	ft, ok := filter.(map[string][]string)
+	ft, ok := filter.(map[string][][]string)
 	if !ok {
-		return fmt.Errorf("filter must be of type map[string][]string, got %T. Expected format: map[role][]fieldValues, where role is the key and fieldValues is a slice of strings. For unused attributes, use an empty string", filter)
+		return fmt.Errorf("filter must be of type map[string][][]string, got %T. "+
+			"Expected format: map[role][][]fieldValues, "+
+			"where role is the key (like 'p', 'g', 'g2') and fieldValues is a 2D slice of strings representing multiple conditions with OR relationship between them. "+
+			"For unused attributes in conditions, use an empty string", filter)
 	}
 	var lines [][]string
 	for k, v := range ft {
-		tmp, err := a.store.selectWhere(ctx, k, 0, v...)
-		if err != nil {
-			return err
+		for _, vv := range v {
+			tmp, err := a.store.selectWhere(ctx, k, 0, vv...)
+			if err != nil {
+				return err
+			}
+			lines = append(lines, tmp...)
 		}
-		lines = append(lines, tmp...)
 	}
+	lines = lo.UniqBy(lines, func(line []string) string {
+		return strings.Join(line, ",")
+	})
 	if len(lines) == 0 {
 		return nil
 	}
